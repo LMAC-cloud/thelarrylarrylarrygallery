73d0c7c9a36a3f4a30e2700880ac9f958e6e6e93
LMAC-cloud <resartus250@gmail.com>
2026-08-12 11:04:49 -0400

    Add files via upload

---
+++ larAudio.js
@@ -0,0 +1,8 @@
+// Radio Laurentis audio playlist
+
+window.LAR_AUDIO_TRACKS = [
+  "Audio/Drumbo.mp3",
+  "Audio/KingLortTheThird.mp3",
+  "Audio/OmsBurnOne.mp3",
+  "Audio/TheFloatingSong.mp3"
+];
